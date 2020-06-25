f4a4aa9c366baec76b942285a85d0f88750e526b
Robert Westbrook <robert_westbrook@alumni.baylor.edu>
2020-06-25 12:11:45 -0400

    Bug Fixed and is working.
    Also alterd code to try - except - finally statement to manage errors more efficiently and to refrain from inserting incomplete or faulty data into db.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+import sys
+from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -8,6 +9,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class Todo(db.Model):
+    __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
 
@@ -15,24 +17,34 @@ class Todo(db.Model):
         return f'<Todo {self.id} {self.description}>'
 
 
-db.create_all() # allows us to call on db.session  
-
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
-    # define variable with the request info  
-    description = request.get_json()['description']
-    todo = Todo(description=description) #creates an instance of the Todo class and pass a value.
-    db.session.add(todo) # Adds 'todo' item, which is INPUT, to pending. 
-    db.session.commit() #commits the User input into db.
-    return jsonify({
-        'description': todo.description
-    })
-
+    error=False
+    body = {}
+    try: # Try - Except - Finally statements are good practice to making testing and properly dealing 
+         # with errors statements.
+        description = request.get_json()['description']# define variable with the request info 
+        todo = Todo(description=description) #creates an instance of the Todo class and pass a value.
+        db.session.add(todo) # Adds 'todo' item, which is INPUT, to pending. 
+        db.session.commit() #commits the User input into db.
+        body['description'] = todo.description
+    except:
+        db.session.rollback()
+        error=True
+        print(sys.exc_info)
+    finally:
+        db.session.close()  
+    if error:
+        abort (400)
+    else:
+        return jsonify(body) # need to call the instance of todo before except statement because 
+                             # an error will cause commit to expire and throw an error whentrying
+                             # to access after the statement. 
 
 @app.route('/')
 def index():
     return render_template('index.html', data=Todo.query.all())
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
