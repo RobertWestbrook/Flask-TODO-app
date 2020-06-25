8c54b384b9c26db13245fd782d759b52ce39310f
Robert Westbrook <robert_westbrook@alumni.baylor.edu>
2020-06-25 16:39:53 -0400

    Added migrations for Database managment

--- app.py
+++ app.py
@@ -1,21 +1,23 @@
 import sys
 from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://roberts@localhost:5432/todoapp'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 class Todo(db.Model):
-    __tablename__ = 'todos'
+# __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
-
+    complete = db.Column(db.Boolean(), nullable=False, default= False)
+    notes = db.Column(db.String(), nullable=True)
+    
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
-
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
@@ -46,5 +48,5 @@ def index():
     return render_template('index.html', data=Todo.query.all())
 
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
