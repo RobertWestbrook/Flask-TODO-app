11e00cfeab1b9d1ead09fe31838ab5ba00b90d86
Robert Westbrook <robert_westbrook@alumni.baylor.edu>
2020-06-29 17:00:09 -0400

    Added multiple list function.
    -Added some styling.
    -Need to fix create list function and not null error on 'todo Item creation'
    -Need to fix 'subscript error' on create list function.

--- app.py
+++ app.py
@@ -10,14 +10,21 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 class Todo(db.Model):
-    # __tablename__ = 'todos'
+    __tablename__ = 'todo'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
     complete = db.Column(db.Boolean(), nullable=False, default= False)
     notes = db.Column(db.String(), nullable=True)
-    
+    todolist_id = db.Column(db.Integer, db.ForeignKey('todolists.id'), nullable= False)
+
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
+
+class TodoList(db.Model):
+    __tablename__ = 'todolists'
+    id = (db.Column(db.Integer, primary_key=True))
+    name = (db.Column(db.String(), nullable=False))
+    todos = db.relationship('Todo', backref='list', lazy=True)
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
@@ -26,14 +33,14 @@ def create_todo():
     try: # Try - Except - Finally statements are good practice to making testing and properly dealing 
          # with errors statements.
         description = request.get_json()['description']# define variable with the request info 
-        todo = Todo(description=description, completed=False) #creates an instance of the Todo class and pass a value.
+        todo = Todo(description=description, complete=False) #creates an instance of the Todo class and pass a value.
         db.session.add(todo) # Adds 'todo' item, which is INPUT, to pending. 
         db.session.commit() #commits the User input into db.
         body['description'] = todo.description
     except:
         db.session.rollback()
         error=True
-        print(sys.exc_info)
+        print(sys.exc_info())
     finally:
         db.session.close()  
     if error:
@@ -67,10 +74,37 @@ def delete_todo(todo_id):
         db.session.close()
     return jsonify({'success': True })
 
+@app.route('/lists/<todolist_id>')
+def get_list_todos(todolist_id):
+    return render_template('index.html', 
+    lists=TodoList.query.all(),
+    active_list=TodoList.query.get(todolist_id),
+    todos=Todo.query.filter_by(todolist_id=todolist_id).order_by('id').all())
+
+@app.route('/lists/create', methods=['POST']) 
+def create_list():
+    error= False
+    body= {}
+    try:
+        name = request.get_json()['name']
+        todolist = TodoList(name=name, complete=False)
+        db.session.add(todolist)
+        db.session.commit()
+        body['name'] = todolist.name
+    except:
+        db.session.rollback()
+        error = True
+        print(sys.exc_info())
+    finally:
+        db.session.close()  
+    if error:
+        abort (400)
+    else:
+        return jsonify(body)
+
 @app.route('/')
 def index():
-    return render_template('index.html', data=Todo.query.order_by('id').all())
-
+    return redirect(url_for('get_list_todos', todolist_id=1))
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
